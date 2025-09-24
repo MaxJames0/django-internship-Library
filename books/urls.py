dd4b0ef2537786c5a7d3e08e58f9c4fe4eb4b635
unknown <ali.alipanah100@gmail.com>
2025-09-24 23:30:34 +0330

    book update and delete

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,9 @@ from .views import *
 app_name = 'books'
 
 urlpatterns = [
-    path('' , BookList.as_view() , name='books-list-dashboard' ),
+    path('' , BookDashboardList.as_view() , name='books-list-dashboard' ),
     path('dashboard/create', BookDashboardCreate.as_view(), name='create-book-dashboard'),
+    path('dashboard/update/<int:pk>/', BookDashboardUpdate.as_view(), name='update-book-dashboard'),
+    path('dashboard/delete/<int:pk>/', BookDashboardDelete.as_view(), name='delete-book'),
     
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from pyexpat import model
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 from books.forms import *
 from books.models import *
@@ -9,19 +10,11 @@ from books.models import *
 # Create your views here.
 
 
-class BookList(ListView):
+class BookDashboardList(ListView):
     template_name = "books/books_list_dashboard.html"
     model = Book
     context_object_name = 'books'
-    paginate_by = 2
-    
-    def get_queryset(self):
-        queryset = super().get_queryset().order_by('-created_at')
-        query = self.request.GET.get('q')
-        if query:
-            queryset = queryset.filter(title__icontains = query) | queryset.filter(content__icontains = query)
-        return queryset
-    
+
     
 class BookDashboardCreate(CreateView):
     template_name = 'books/book_create_dashboard.html'
@@ -32,3 +25,26 @@ class BookDashboardCreate(CreateView):
     def form_valid(self, form):
         form.instance.added_by = self.request.user
         return super().form_valid(form)
+    
+
+class BookDashboardUpdate(UpdateView):
+    template_name = "books/book_update_dashboard.html"
+    model = Book
+    form_class = BookForm
+    success_url = reverse_lazy('books:books-list-dashboard')
+
+    def form_valid(self, form):
+        # keep the existing added_by user or update if you want
+        if not form.instance.added_by:
+            form.instance.added_by = self.request.user
+        return super().form_valid(form)
+    
+    def form_valid(self, form):
+        print("Form submitted")
+        return super().form_valid(form)
+    
+
+class BookDashboardDelete(DeleteView):
+    template_name = "books/book_delete_confirm_dashboard.html"
+    model = Book
+    success_url = reverse_lazy('books:books-list-dashboard')
